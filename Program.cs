b382fdf785b72ea926f49ffa3d44eeca676c3853
Denis Zadniprovskiy <dzadnip@gmail.com>
2017-12-28 14:05:28 -0800

    dotnet new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Programming_Guide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
